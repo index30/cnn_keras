8b2b7b02820919165d684aa957a3695cb8e7eeec
ranthy <realthingshakes25@gmail.com>
2018-05-10 17:02:34 +0900

    modify test code and so on about it.

--- cnn_arch/lenet_model.py
+++ cnn_arch/lenet_model.py
@@ -44,18 +44,43 @@ def build_model(CLASSES=2, IMAGE_SIZE=128, b_show=False, b_gray=False):
         model.summary()
     return model
 
+def model_compile(model,
+                  LOSS='categorical_crossentropy',
+                  OPTIMIZER='adam',
+                  METRICS=['acc']):
+    model.compile(
+        loss=LOSS,
+        optimizer=OPTIMIZER,
+        metrics=METRICS
+    )
+    return model
+
+def batch_build(CLASSES=2,
+                IMAGE_SIZE=128,
+                b_show=False,
+                b_gray=False,
+                LOSS='categorical_crossentropy',
+                OPTIMIZER='adam',
+                METRICS=['acc']):
+    model = build_model(CLASSES=CLASSES,
+                        IMAGE_SIZE=IMAGE_SIZE,
+                        b_show=b_show,
+                        b_gray=b_gray)
+    model = model_compile(model,
+                          LOSS=LOSS,
+                          OPTIMIZER=OPTIMIZER,
+                          METRICS=METRICS)
+    return model
+
 
 """build_model_immutable
 This method is similar to build_model,
 but this method return certain model
 """
-def build_model_immutable(CLASSES=2, IMAGE_SIZE=128, b_show=False, b_gray=False):
+def build_model_immutable():
     # Definition of model
     model = Sequential()
-    if b_gray:
-        input_shape = (IMAGE_SIZE, IMAGE_SIZE, 1)
-    else:
-        input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)
+    input_shape = (128, 128, 3)
     # 第一層
     model.add(Conv2D(32, (5, 5), input_shape=input_shape))
     model.add(Activation('relu'))
@@ -69,8 +94,7 @@ def build_model_immutable(CLASSES=2, IMAGE_SIZE=128, b_show=False, b_gray=False)
     model.add(Dense(128))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(CLASSES))
+    model.add(Dense(2))
     model.add(Activation('softmax'))
-    if b_show:
-        model.summary()
+
     return model
--- tests/test_lenet_model.py
+++ tests/test_lenet_model.py
@@ -1,7 +1,15 @@
+import numpy as np
+from numpy.testing import assert_allclose
+import os
 from pathlib import Path
+from skimage.io import imread
 import sys
+import tempfile
 import unittest
 from unittest import TestCase
+
+from keras.utils import np_utils
+from keras.models import save_model, load_model
 
 sys.path.append(str(Path(Path.cwd().parents[0])))
 from cnn_arch import lenet_model
@@ -23,18 +31,48 @@ class TestLeNet(TestCase):
 
 
     def test_build_model_immutable(self):
-        """Check Error
-            When CLASSES has wrong arg, assert TypeError
-            When IMAGE_SIZE has wrong arg, assert ValueError
+        pass
+
+
+    """
+    test whether training model's result and saving model's result is match
+    """
+    def test_model_saving(self):
+        # build and compile models
+        # build from lenet_model.py, build_model_immutable
+        model = lenet_model.build_model_immutable()
+        # compile model
+        model.compile(
+            loss='categorical_crossentropy',
+            optimizer='adam',
+            metrics=['acc']
+        )
+
+        # Get path of image for training
+        data_path = Path(Path.cwd().parent, 'data/tests').glob('*.jpg')
+        """data_path
+        dog.jpg   ...label 0
+        cat1~3.jpg...label 1
         """
-        # test_arg is the pattern of error at arg in build_model
-        wrong_arg = [
-            ("classes", "b_show")
-        ]
-        with self.assertRaises(TypeError):
-            lenet_model.build_model_immutable(CLASSES=wrong_arg[0][0])
-        with self.assertRaises(ValueError):
-            lenet_model.build_model_immutable(IMAGE_SIZE=wrong_arg[0][1])
+        X = [imread(str(path)) for path in data_path]
+        X = np.array(X)
+        y = np.array([1, 1, 1, 0])
+        # convert label to one-hot label
+        y = np_utils.to_categorical(y)
+        # training
+        model.train_on_batch(X, y)
+
+        out = model.predict(X[0][np.newaxis, :])
+        _, fname = tempfile.mkstemp('.h5')
+        save_model(model, fname)
+
+        new_model = load_model(fname)
+        os.remove(fname)
+
+        out2 = new_model.predict(X[0][np.newaxis, :])
+        print(out)
+        print(out2)
+        assert_allclose(out, out2, atol=1e-05)
 
 
 if __name__=="__main__":
